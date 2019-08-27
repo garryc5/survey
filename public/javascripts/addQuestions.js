e6d20fbbaf947a38e966fbdc539f2a97e8d4e944
gcarver <gcarver@gcarvers-MacBook-Pro.local>
2019-08-27 15:53:56 -0500

    Pie Chart Done

--- public/javascripts/addQuestions.js
+++ public/javascripts/addQuestions.js
@@ -2,13 +2,13 @@ var q = 1;
 function addQuestions(){
 var questions = document.getElementById('lastQuestion');
 questions.appendChild(document.createElement("Div")).innerHTML=
-    '<label>Question:'+
-    `<input type="text" name="question${q}" placeholder="Required" required></label>`+
-    '<label>Answers:'+
-    `<input type="text" name="a${q}0" placeholder="Required" required>`+
-    `<input type="text" name="a${q}1" placeholder="Required" required><`+
-    `<input type="text" name="a${q}2" placeholder="Optional" >`+
-    `<input type="text" name="a${q}3" id="lastQuestion" placeholder="Optional" ></label>`
+    '<br><label>Question:'+
+    `<input type="text" name="question${q}" placeholder="Required" required></label><br>`+
+    '<label>Answers: <br>'+
+    `<input type="text" name="a${q}0" placeholder="Required" required>  `+
+    `<input type="text" name="a${q}1" placeholder="Required" required>  `+
+    `<input type="text" name="a${q}2" placeholder="Optional">  `+
+    `<input type="text" name="a${q}3" placeholder="Optional"></label>  `
 ;
 q++;
 }
